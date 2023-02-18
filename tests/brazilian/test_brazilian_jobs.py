1e2cd843ee0f143cab5d425b85286b3215fe72de
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-02-18 01:46:54 -0300

    fix: Tentando resolve requisito 11 (1)

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,7 +1,5 @@
 from src.pre_built.brazilian_jobs import read_brazilian_file
-from tests.brazilian.mocks import brazilian_mock
 
 
 def test_brazilian_jobs():
-    assert read_brazilian_file(
-        "tests/mocks/brazilians_jobs.csv") == brazilian_mock
+    assert "title" in read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0]
