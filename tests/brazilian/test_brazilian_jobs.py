da43bd24f29c049e610d821930c45b19732d2333
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-02-18 01:28:20 -0300

    feat: Requisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,7 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from tests.brazilian.mocks import brazilian_mock
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file(
+        "tests/mocks/brazilians_jobs.csv") == brazilian_mock
