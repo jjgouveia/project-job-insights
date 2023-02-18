0aa4ba222b4f51fea8fefb938af4ba4819b75103
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-02-18 00:31:36 -0300

    feat: Requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    '''Testa a ocorrência das palavras Python e Javascript no arquivo
+    informado '''
+    assert count_ocurrences('data/jobs.csv', 'Python') == 1639
+    assert count_ocurrences('data/jobs.csv', 'Javascript') == 122
